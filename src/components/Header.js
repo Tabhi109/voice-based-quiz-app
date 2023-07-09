8626d09db3702a85ad4b351120b0776d214dba93
Tabhi109 <tabhinav109@gmail.com>
2023-07-09 21:05:44 +0530

    added logic for the modal to open and close

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Flex, Heading, Button } from "@chakra-ui/react";
+import Modal from "./Modal";
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Box bg="navy" color="white" p={4}>
       <Flex align="center" justify="space-between">
         <Heading as="h1" size="xl">
           TechTrek
         </Heading>
-        <Button colorScheme="teal" variant="solid">
+        <Button colorScheme="teal" variant="solid" onClick={handleOpenModal}>
           Start a Quiz
         </Button>
       </Flex>
+      {isModalOpen && <Modal onClose={handleCloseModal} />}
     </Box>
   );
 };
